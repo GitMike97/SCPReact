1e236d1642546c54135e50c3657fe5db6664e0f6
GitMike97 <89360554+GitMike97@users.noreply.github.com>
2022-04-06 17:59:04 +1200

    fifth commit

--- src/Nav.js
+++ src/Nav.js
@@ -11,7 +11,7 @@ function Nav()
                 <img src = {Image} style={{height:'50px', width:'100px'}} alt="Logo"/>
             </div>
             
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <Link to="/" className="nav-item" style={{textDecoration: 'none'}}>
